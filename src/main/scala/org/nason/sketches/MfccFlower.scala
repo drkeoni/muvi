9743df1a213b74dd4599ddbf70523c3555fc2223
drkeoni <jon_m_sorenson@yahoo.com>
2017-02-20 15:07:28 -0800

    moving along with this new version of Lissajous sketch.  It's relaxing, and it's gives some character to the MFCC coefficients.

--- src/main/scala/org/nason/sketches/MfccFlower.scala
+++ src/main/scala/org/nason/sketches/MfccFlower.scala
@@ -27,6 +27,23 @@ class MfccFlower() extends MusicVideoApplet(Some("mfcc/sketch.conf")) {
   val SONG_FILE:String = songFiles(config.getString("song.name"))
   val BG_COLOR:Int = color(confFloat("sketch.background.r"),confFloat("sketch.background.g"),confFloat("sketch.background.b"))
 
+  val NUM_BALLS = config.getInt("mfcc.num_balls")
+  val INITIAL_RADIUS = config.getInt("mfcc.initial_radius")
+  val BALL_RADIUS = config.getInt("mfcc.ball_radius")
+  val PALETTE_MINH = config.getInt("mfcc.palette.min.h")
+  val PALETTE_MAXH = config.getInt("mfcc.palette.max.h")
+  val PALETTE_MINS = config.getInt("mfcc.palette.min.s")
+  val PALETTE_MAXS = config.getInt("mfcc.palette.max.s")
+  val PALETTE_MINV = config.getInt("mfcc.palette.min.v")
+  val PALETTE_MAXV = config.getInt("mfcc.palette.max.v")
+  val ATTRACTION_FORCE = confFloat("mfcc.attraction_force")
+  val MAX_ATTRACTOR_MULT = confFloat("mfcc.max_attractor_mult") //4.0
+  val VELOCITY_MULT = confFloat("mfcc.velocity_mult") //0.9999
+  val VELOCITY_BATH = confFloat("mfcc.velocity_bath") //0.05
+  val ALPHA_MULT = confFloat("mfcc.alpha_mult") //0.95
+  val RADIUS_MULT = confFloat("mfcc.radius_mult") //0.95
+  val MAX_AGE = config.getInt("mfcc.max_age") //40
+
   override def setup():Unit = {
 
     physics = new VerletPhysics()
@@ -53,11 +70,12 @@ class MfccFlower() extends MusicVideoApplet(Some("mfcc/sketch.conf")) {
   class MfccLayout( physics:VerletPhysics ) extends Agent {
 
     val balls:ArrayBuffer[MfccBall] = {
-      val palette = Color.hsvSeries(Seq(0,359,70,70,80,80),12).map( c => color(c._1,c._2,c._3) )
+      val palette = Color.hsvSeries(Seq(PALETTE_MINH,PALETTE_MAXH,PALETTE_MINS,
+        PALETTE_MAXS,PALETTE_MINV,PALETTE_MAXV),NUM_BALLS).map( c => color(c._1,c._2,c._3) )
 
-      val _b = (0 until 12).map(i => {
-        val theta = 2.0 * Math.PI * i / 12.0
-        new MfccBall(150, theta, palette(i))
+      val _b = (0 until NUM_BALLS).map(i => {
+        val theta = 2.0 * Math.PI * i / NUM_BALLS.toDouble
+        new MfccBall(INITIAL_RADIUS, theta, palette(i))
       })
       _b.foreach(b => physics.addParticle(b.mass))
       val _b2 = new ArrayBuffer[MfccBall]
@@ -98,7 +116,7 @@ class MfccFlower() extends MusicVideoApplet(Some("mfcc/sketch.conf")) {
     def cull() = {
       for( i <- balls.length-1 to 0 by -1 ) {
         val b = balls(i)
-        if ( b.age>100 ) {
+        if ( b.age>MAX_AGE ) {
           balls.remove(i)
           physics.removeParticle(b.mass)
         }
@@ -108,11 +126,11 @@ class MfccFlower() extends MusicVideoApplet(Some("mfcc/sketch.conf")) {
 
   class MfccBall( r:Int, theta:Double, color:Int ) {
     var age:Int = 0
-    private val R0 = 40
-    private val DEFAULT_ALPHA = 255.0f
-    private val CENTER_ATTRACTION = 0.3f
+    private val R0 = BALL_RADIUS
+    private val DEFAULT_ALPHA = 200.0f
+    private val CENTER_ATTRACTION = ATTRACTION_FORCE // 0.3f
 
-    protected var alpha = DEFAULT_ALPHA
+    //protected var alpha = DEFAULT_ALPHA
 
     class BallMass(loc:Vec3D, vel:Vec3D) extends VerletParticle(loc) {
       setWeight(2.5f)
@@ -133,6 +151,8 @@ class MfccFlower() extends MusicVideoApplet(Some("mfcc/sketch.conf")) {
       b.mass.set( this.mass )
       b.mass.clearVelocity()
       b.mass.addVelocity( this.mass.getVelocity )
+      b.mass.setWeight( 1.0f )
+      //b.mass.setAttractor( this.mass, MAX_ATTRACTOR_MULT*r, CENTER_ATTRACTION )
       b.age = this.age+1
       b
     }
@@ -146,21 +166,23 @@ class MfccFlower() extends MusicVideoApplet(Some("mfcc/sketch.conf")) {
     val mass:BallMass = {
       val (x,y) = toXY()
       val _m = new BallMass( new Vec3D(x,y,0.0f), new Vec3D(0.0f,0.0f,0.0f) )
-      _m.setAttractor( new Vec3D(0f,0f,0f), 4.0f*r, CENTER_ATTRACTION )
+      _m.setAttractor( new Vec3D(0f,0f,0f), MAX_ATTRACTOR_MULT*r, CENTER_ATTRACTION )
       _m
     }
 
     def draw() = {
       val (x,y) = (mass.x,mass.y)
-      alpha = (DEFAULT_ALPHA * Math.pow(0.95,age)).toInt
+      val alpha = (DEFAULT_ALPHA * Math.pow(ALPHA_MULT,age)).toInt
+      val radius2 = (2.0*R0 * Math.pow(RADIUS_MULT,age)).toInt
       fill(color,alpha)
       noStroke()
-      ellipse(x,y,R0,R0)
+      ellipse(x,y,radius2,radius2)
 
-      val dvx = randomGaussian()*0.05f
-      val dvy = randomGaussian()*0.05f
+      val dvx = randomGaussian()*VELOCITY_BATH
+      val dvy = randomGaussian()*VELOCITY_BATH
 
       mass.addVelocity( new Vec3D(dvx,dvy,0.0f) )
+      mass.scaleVelocity( VELOCITY_MULT )
       //logger.info("ellipse at (%f,%f,%d,%d)".format(x,y,R0,R0))
     }
   }

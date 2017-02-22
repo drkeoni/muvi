74260e67e65fb1700fdbc192beef568e2cd209e2
drkeoni <jon_m_sorenson@yahoo.com>
2017-02-21 19:37:23 -0800

    significant improvements everywhere; nice working sketch of groups of flocks.

--- src/main/scala/org/nason/sketches/BoidSketch.scala
+++ src/main/scala/org/nason/sketches/BoidSketch.scala
@@ -6,7 +6,6 @@ import org.nason.util.Color
 import processing.core.PApplet
 import toxi.geom.Vec3D
 import toxi.physics.{VerletParticle, VerletPhysics}
-import toxi.physics.behaviors.GravityBehavior
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -29,12 +28,34 @@ class BoidSketch() extends MusicVideoApplet(Some("boids/sketch.conf")) {
 
   val NUM_BOIDS = config.getInt("boids.n")
   val BIRD_SIZE = config.getInt("boids.boid_size")
+  val NUM_GROUPS = config.getInt("boids.num_groups")
+  val BOUNDING_BOX_SIZE = config.getInt("boids.bounding_box_size")
+
   val PALETTE_MINH = config.getInt("boids.palette.min.h")
   val PALETTE_MAXH = config.getInt("boids.palette.max.h")
   val PALETTE_MINS = config.getInt("boids.palette.min.s")
   val PALETTE_MAXS = config.getInt("boids.palette.max.s")
   val PALETTE_MINV = config.getInt("boids.palette.min.v")
   val PALETTE_MAXV = config.getInt("boids.palette.max.v")
+  val COLOR_BY_GROUP = config.getBoolean("boids.color_by_group")
+
+  val START_MINX = config.getInt("boids.start.min.x")
+  val START_MAXX = config.getInt("boids.start.max.x")
+  val START_MINVX = config.getInt("boids.start.minv.x")
+  val START_MAXVX = config.getInt("boids.start.maxv.x")
+  val START_MINY = config.getInt("boids.start.min.y")
+  val START_MAXY = config.getInt("boids.start.max.y")
+  val START_MINVY = config.getInt("boids.start.minv.y")
+  val START_MAXVY = config.getInt("boids.start.maxv.y")
+
+  val CM_FACTOR = confFloat("boids.rule_factors.cm")
+  val GOAL_FACTOR = confFloat("boids.rule_factors.goal")
+  val AVOID_FACTOR = confFloat("boids.rule_factors.avoid_boids")
+  val WALL_FACTOR = confFloat("boids.rule_factors.avoid_walls")
+  val ALIGN_FACTOR = confFloat("boids.rule_factors.align")
+  val BATH_FACTOR = confFloat("boids.rule_factors.bath")
+  val OBSTACLE_COLOR = color(confFloat("boids.obstacle_color.r"),confFloat("boids.obstacle_color.g"),confFloat("boids.obstacle_color.b"))
+  val AVOID_SQ = config.getInt("boids.avoid_sq")
 
   override def setup():Unit = {
     physics = new VerletPhysics()
@@ -60,23 +81,143 @@ class BoidSketch() extends MusicVideoApplet(Some("boids/sketch.conf")) {
 
   class BoidsManager(physics:VerletPhysics) extends Agent {
 
-    val boids:Array[Boid] = {
-      val _b = new ArrayBuffer[Boid]()
-      val palette = Color.hsvSeries(Seq(PALETTE_MINH,PALETTE_MAXH,PALETTE_MINS,
-        PALETTE_MAXS,PALETTE_MINV,PALETTE_MAXV),NUM_BOIDS).map( c => color(c._1,c._2,c._3) )
+    var currentGoal:Array[Int] = Array.fill[Int](NUM_GROUPS)(0)
+    val GOALS = (0 until 12).map( i => {
+      val th = Math.PI * 2.0 * i / 12.0
+      new Vec3D((800.0*Math.cos(th)).toFloat,(300.0*Math.sin(th)).toFloat,0f)
+    })
 
-      for( i <-0 until NUM_BOIDS ) {
-        val (bx, by) = (random(-200, 100), random(-100, 200))
-        val (vx, vy) = (random(-1, 1), random(-1, 1))
-        _b += new Boid(bx.toInt, by.toInt, vx.toInt, vy.toInt, color(palette(i)) )
+    val groups = (0 until NUM_GROUPS).map( i => new BoidsGroup(i) )
+
+    class BoidsGroup(id:Int) {
+      val boids:Array[Boid] = {
+        val _b = new ArrayBuffer[Boid]()
+        val nPalette = if (COLOR_BY_GROUP) NUM_GROUPS else NUM_BOIDS
+        val palette = Color.hsvSeries(Seq(PALETTE_MINH,PALETTE_MAXH,PALETTE_MINS,
+          PALETTE_MAXS,PALETTE_MINV,PALETTE_MAXV),nPalette).map( c => color(c._1,c._2,c._3) )
+
+        for( i <-0 until NUM_BOIDS ) {
+          val (bx, by) = (random(START_MINX,START_MAXX), random(START_MINY,START_MAXY))
+          val (vx, vy) = (random(START_MINVX,START_MAXVX), random(START_MINVX,START_MAXVX))
+          val j = if (COLOR_BY_GROUP) id else i
+          _b += new Boid(bx.toInt, by.toInt, vx.toInt, vy.toInt, color(palette(j)) )
+        }
+        _b.foreach( b => physics.addParticle(b.mass) )
+        _b.toArray
       }
-      val cv = new Vec3D(0f,0f,0f)
-      _b.foreach( b => cv.add(b.mass.getVelocity) )
-      cv.scale(-1.0f/_b.length.toFloat)
-      _b.foreach( b => b.mass.addVelocity(cv) )
-      _b.foreach( b => physics.addParticle(b.mass) )
-      _b.toArray
+
+      def applyRules() = {
+        val r1factor = CM_FACTOR
+        val goalFactor = GOAL_FACTOR
+        val r2factor = AVOID_FACTOR
+        val r3factor = ALIGN_FACTOR
+        val temperature = BATH_FACTOR
+        //
+        // tabulate neighbors
+        //
+        boids.foreach( b => b.clearNeighbors() )
+
+        val DIST_BASED_NEIGHBORS = config.getBoolean("boids.distance_based_neighbors")
+
+        if ( DIST_BASED_NEIGHBORS ) {
+          for (i <- 0 until boids.length; j <- i + 1 until boids.length) {
+            val bi = boids(i)
+            val bj = boids(j)
+            if (bi.mass.distanceToSquared(bj.mass) < 45 * BIRD_SIZE * BIRD_SIZE) {
+              bi.addNeighbor(bj)
+              bj.addNeighbor(bi)
+            }
+          }
+        } else {
+          for( i <- 0 until boids.length ) {
+            val bi = boids(i)
+            for( j <- -30 to 30 ) {
+              val jj = ( j + boids.length ) % boids.length
+              bi.addNeighbor( boids(jj) )
+            }
+          }
+        }
+        //
+        // center of mass
+        //
+        boids.filter( _.neighbors.length>0 ).foreach( b => {
+          val cm = new Vec3D(0f,0f,0f)
+          b.neighbors.foreach( b => cm.add(b.mass) )
+          cm.scale( 1f/b.neighbors.length.toFloat )
+          val delta = cm.sub(b.mass).normalize.scale(r1factor)
+          b.mass.addVelocity(delta)
+        })
+        //
+        // goal
+        //
+        val goal = GOALS(currentGoal(id))
+        boids.foreach( b => {
+          val delta = goal.copy.sub(b.mass).normalize.scale(goalFactor)
+          b.mass.addVelocity(delta)
+        })
+        //
+        // avoidance
+        //
+        boids.filter( _.neighbors.length>0 ).foreach( bi => {
+          bi.neighbors.foreach(bj => {
+            if (bi.mass.distanceToSquared(bj.mass) < AVOID_SQ * BIRD_SIZE * BIRD_SIZE) {
+              val bij = bj.mass.copy.sub(bi.mass).normalize
+              bi.mass.addVelocity(bij.scale(-r2factor))
+            }
+          })
+        })
+        //
+        // bounding box
+        //
+        val lx0 = new Vec3D(-BOUNDING_BOX_SIZE,-BOUNDING_BOX_SIZE,0)
+        val lx1 = new Vec3D(BOUNDING_BOX_SIZE,-BOUNDING_BOX_SIZE,0)
+        val lx2 = new Vec3D(BOUNDING_BOX_SIZE,BOUNDING_BOX_SIZE,0)
+        val lx3 = new Vec3D(-BOUNDING_BOX_SIZE,BOUNDING_BOX_SIZE,0)
+        boids.foreach( b => {
+          b.avoidLine( lx0, lx1, 9*BIRD_SIZE*BIRD_SIZE, WALL_FACTOR )
+          b.avoidLine( lx1, lx2, 9*BIRD_SIZE*BIRD_SIZE, WALL_FACTOR )
+          b.avoidLine( lx2, lx3, 9*BIRD_SIZE*BIRD_SIZE, WALL_FACTOR )
+          b.avoidLine( lx3, lx0, 9*BIRD_SIZE*BIRD_SIZE, WALL_FACTOR )
+        })
+        //
+        // obstacle
+        //
+        val ly0 = new Vec3D(-190,0,0)
+        val ly1 = new Vec3D(190,0,0)
+        boids.foreach( b => {
+          b.avoidLine(ly0, ly1, 4*BIRD_SIZE * BIRD_SIZE, WALL_FACTOR )
+        })
+        //
+        // alignment
+        //
+        boids.filter( _.neighbors.length>0 ).foreach( b => {
+          val cv = new Vec3D(0f,0f,0f)
+          b.neighbors.foreach( b => cv.add(b.mass.getVelocity) )
+          val deltav = cv.scale(1f/b.neighbors.length.toFloat).sub(b.mass.getVelocity).normalize
+          b.mass.addVelocity( deltav.scale(r3factor) )
+        })
+        //
+        // bath fluctuations
+        //
+        boids.foreach( b => {
+          val vx = randomGaussian() * temperature
+          val vy = randomGaussian() * temperature
+          b.mass.addVelocity(new Vec3D(vx, vy, 0f))
+        })
+        //
+        // RESCALE
+        //
+        boids.foreach( b => {
+          val vf = b.mass.getVelocity.magnitude
+          b.mass.scaleVelocity( 1.0f/vf )
+        })
+      }
+
+      def draw() = boids.foreach( _.draw )
     }
+
+    def applyRules():Unit = groups.foreach( _.applyRules() )
+
     /**
       * Agents are objects in the system which care about the external environment surrounding the music
       * piece.
@@ -88,65 +229,43 @@ class BoidSketch() extends MusicVideoApplet(Some("boids/sketch.conf")) {
       * @param signals map of arbitrary keys to arrays of floats
       * @param events  map of arbitrary keys to VideoEvent objects
       */
-    override def processEnvironment(time: Float, signals: Map[String, Array[Float]], events: Map[String, VideoEvent]): Unit = {}
-
-    def applyRules() = {
-      val r1factor = 0.02f
-      val r2factor = 0.04f
-      val r3factor = 0.0001f
-      //
-      // center of mass
-      // TODO: make this local
-      //
-      val cm = new Vec3D(0f,0f,0f)
-      boids.foreach( b => cm.add(b.mass) )
-      val sf = 1.0f/(boids.length-1).toFloat
-      boids.foreach( b => {
-        val my_cm = cm.copy.sub(b.mass).scale(sf)
-        val delta = my_cm.sub(b.mass).normalize.scale(r1factor)
-        b.mass.addVelocity(delta)
-      })
-      //
-      // avoidance
-      //
-      for( i <- 0 until boids.length; j <- i+1 until boids.length ) {
-        val bi = boids(i)
-        val bj = boids(j)
-        if ( bi.mass.distanceToSquared(bj.mass) < 9 * BIRD_SIZE*BIRD_SIZE ) {
-          val bij = bj.mass.copy.sub(bi.mass).normalize
-          val bij1 = bij.copy.scale(-1f)
-          bi.mass.addVelocity( bij1.scale(r2factor) )
-          bj.mass.addVelocity( bij.scale(r2factor) )
-        }
+    override def processEnvironment(time: Float, signals: Map[String, Array[Float]], events: Map[String, VideoEvent]): Unit = {
+      val ti = (time/2f).toInt % 12
+      for( i<-0 until NUM_GROUPS ) {
+        currentGoal(i) = ( ti + 3 * i ) % 12
       }
-      //
-      // alignment
-      // TODO : make this local
-      //
-      val cv = new Vec3D(0f,0f,0f)
-      boids.foreach( b => cv.add(b.mass.getVelocity) )
-      boids.foreach( b => {
-        val my_cv = cv.copy.sub(b.mass.getVelocity)
-        val deltav = my_cv.scale(sf).sub(b.mass.getVelocity.copy).normalize
-        b.mass.addVelocity( deltav.scale(r3factor) )
-      })
 
+      /*val mfcc = signals("mfcc").slice(2,13)
+      val mfccs = mfcc.sorted
+      val m0 = mfccs(mfccs.length-1)
+      val mfcc0 = mfcc.indexWhere( s => s==m0 )
+      currentGoal = mfcc0
+      if ( currentGoal < 0 )
+        currentGoal = 0*/
     }
 
-    def draw() = {
+    def draw():Unit = {
       pushMatrix()
       translate(width/2,height/2)
-      boids.foreach( _.draw )
+      groups.foreach( _.draw )
+      //fill(color(220,180,30),200f)
+      fill(OBSTACLE_COLOR,200f)
+      noStroke()
+      rect(-200,-10,400,20)
+
+      //fill(color(220,180,255),200f)
+      //val goal = GOALS(currentGoal)
+      //ellipse(goal.x-50,goal.y-50,100,100)
+
       popMatrix()
     }
 
-    def cull() = {}
+    def cull():Unit = {}
   }
 
   class Boid(x:Int,y:Int,vx:Int,vy:Int,_color:Int) {
-    class BoidMass(loc:Vec3D) extends VerletParticle(loc) {
+    class BoidMass(loc:Vec3D) extends VerletParticle(loc)
 
-    }
     val mass = {
       val _m = new BoidMass(new Vec3D(x.toFloat,y.toFloat,0.0f))
       _m.addVelocity(new Vec3D(vx,vy,0))
@@ -164,5 +283,38 @@ class BoidSketch() extends MusicVideoApplet(Some("boids/sketch.conf")) {
       triangle(x0.toInt,y0.toInt,x2.toInt,y2.toInt,x1.toInt,y1.toInt)
       //logger.info("drawing at (%d,%d,%d,%d,%d,%d)".format(x0.toInt,y0.toInt,x2.toInt,y2.toInt,x1.toInt,y1.toInt))
     }
+
+    val neighbors = new ArrayBuffer[Boid]
+    def clearNeighbors():Unit = neighbors.clear()
+    def addNeighbor(n:Boid):Unit = neighbors += n
+
+    def avoidLine(x0:Vec3D, x1:Vec3D, distSq:Int, avoidFactor:Float=0.07f) = {
+      val r4factor = avoidFactor
+      val d0 = x0.distanceToSquared(this.mass)
+      val d1 = x1.distanceToSquared(this.mass)
+      val n = x1.copy.sub(x0).normalize
+      val rx1 = x1.copy.sub(this.mass)
+      val sig1 = rx1.dot(n)
+      val rx0 = x0.copy.sub(this.mass)
+      val sig2 = rx0.dot(n)
+      val rx3 = rx1.copy.sub( n.copy.scale(rx1.dot(n)) )
+      var d2sq = 1e10
+      if ( sig1>0.0 && sig2<0.0 ) {
+        d2sq = rx3.magSquared()
+      }
+      val minDist = Seq(d0,d1,d2sq).min
+      if (minDist < distSq) {
+        var deltav:Vec3D = null
+        if (minDist==d0) {
+          deltav = x0.copy.sub(this.mass).normalize.scale(-1f)
+        } else if (minDist==d1) {
+          deltav = rx1.copy.normalize.scale(-1f)
+        } else {
+          deltav = rx3.normalize.scale(-1f)
+        }
+        this.mass.addVelocity( deltav.scale(r4factor) )
+      }
+    }
+
   }
 }
